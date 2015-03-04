6f195b109fe8f6f0d7f4f01e058fb07fa896106d
Karl <khughes.me@gmail.com>
2015-03-04 14:20:57 -0600

    Adding attribute setters/getters

--- src/Jobs/Collections/JobsCollection.php
+++ src/Jobs/Collections/JobsCollection.php
@@ -11,12 +11,26 @@ class JobsCollection extends Collection
         $this->setAttributes($attributes);
     }
 
+    public function setAttribute($name, $value)
+    {
+        if ($name && $value) {
+            $this->attributes[$name] = $value;
+        }
+    }
+
     public function setAttributes($attributes = [])
     {
         if ($attributes && is_array($attributes)) {
             foreach ($attributes as $name => $value) {
                 $this->attributes[$name] = $value;
             }
+        }
+    }
+
+    public function getAttribute($name)
+    {
+        if ($name && isset($this->attributes[$name])) {
+            return $this->attributes[$name];
         }
     }
 
