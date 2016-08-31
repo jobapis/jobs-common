87e24288f7b99cf7ceb3e42e047f5f03462d2ed0
Karl <khughes.me@gmail.com>
2016-08-31 17:27:09 -0500

    Updating jobs provider to work with new query class

--- tests/fixtures/ConcreteProvider.php
+++ tests/fixtures/ConcreteProvider.php
@@ -1,19 +1,10 @@
 <?php namespace JobApis\Jobs\Client\Fixtures;
 
-use JobApis\Jobs\Client\Collection;
-use JobApis\Jobs\Client\Exceptions\MissingParameterException;
 use JobApis\Jobs\Client\Job;
 use JobApis\Jobs\Client\Providers\AbstractProvider;
 
 class ConcreteProvider extends AbstractProvider
 {
-    /**
-     * Base API Url
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://api.example.com/';
-
     /**
      * Returns the standardized job object
      *
@@ -34,34 +25,6 @@ class ConcreteProvider extends AbstractProvider
     }
 
     /**
-     * Get default parameters and values
-     *
-     * @return  string
-     */
-    public function defaultParameters() {
-        return [
-            'ipAddress' => $this->currentUserIpAddress(),
-        ];
-    }
-
-    /**
-     * Job object default keys that should be set
-     *
-     * @return  string
-     */
-    public function defaultResponseFields()
-    {
-        return [
-            'id',
-            'name',
-            'company',
-            'date',
-            'snippet',
-            'url',
-        ];
-    }
-
-    /**
      * Get listings path
      *
      * @return  string
@@ -69,59 +32,4 @@ class ConcreteProvider extends AbstractProvider
     public function getListingsPath() {
         return 'jobs';
     }
-
-    /**
-     * Get parameters that MUST be set in order to satisfy the APIs requirements
-     *
-     * @return  string
-     */
-    public function requiredParameters() {
-        return [
-            'keyword',
-            'ipAddress',
-            'affiliateId',
-        ];
-    }
-
-    /**
-     * Get parameters that CAN be set
-     *
-     * @return  string
-     */
-    public function validParameters() {
-        return [
-            'keyword',
-            'ipAddress',
-            'affiliateId',
-            'optionalParam1',
-            'optionalParam2',
-        ];
-    }
-
-    public function currentUserIpAddress()
-    {
-        return uniqid();
-    }
-
-    /**
-     * Get optional parameter. Demonstrates simple transformation on getter.
-     *
-     * @return  string
-     */
-    public function getOptionalParam2()
-    {
-        return strrev($this->optionalParam2);
-    }
-
-    /**
-     * Set optional parameter. Demonstrates simple transformation on setter.
-     *
-     * @return  string
-     */
-    public function setOptionalParam1($value)
-    {
-        $this->optionalParam1 = strrev($value);
-        return $this;
-    }
-
 }
